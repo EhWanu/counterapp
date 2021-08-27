a72a71e152256f39f8ced635587952408929ca71
EhWanu <cameronjshadgett@gmail.com>
2021-08-26 22:22:19 -0400

    CounterHooks shell setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Counter from './Counter';
+import CounterHooks from './CounterHooks'
 
 function App() {
   return (
+    <>
+    Counter
     <Counter initialCount={0} />
+    CounterHooks
+    <CounterHooks initialCount={0} />
+    </>
   )
 }
 
