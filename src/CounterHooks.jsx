e98d8ed9e774436d9fe969cd83b5a71672ba7182
EhWanu <cameronjshadgett@gmail.com>
2021-08-26 22:38:41 -0400

    prevState for asynchronous function

--- src/CounterHooks.jsx
+++ src/CounterHooks.jsx
@@ -4,7 +4,9 @@ export default function CounterHooks({ initialCount}) {
     const [state, setState] = useState({ count: initialCount })
     return (
         <div>
-        <button onClick={() => setState({ count: state.count - 1})}>-</button>
+        <button onClick={() => setState(prevState => { 
+            return{ count: prevState.count - 1}
+        })}>-</button>
         <span>{state.count}</span>
         <button onClick={() => setState({ count: state.count + 1})}>+</button>
     </div>
