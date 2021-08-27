af386979863d1723ec295f0f1bb13745e8993c2f
EhWanu <cameronjshadgett@gmail.com>
2021-08-26 22:28:40 -0400

    Deconstruct State Array

--- src/CounterHooks.jsx
+++ src/CounterHooks.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export default function CounterHooks({ initialCount}) {
-    useState({ count: initialCount })
+    const [state, setState] = useState({ count: initialCount })
     return (
         <div>
         <button>-</button>
